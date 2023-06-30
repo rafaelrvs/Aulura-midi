03ab3ee71c64509b86ceeec4b9775e0d46879e0d
rafaelrvs <rafaelvieira1333@gmail.com>
2023-06-30 20:10:37 -0300

    Update main.js
    Atualização de funcionalidade e correção de bug

--- main.js
+++ main.js
@@ -1,29 +1,38 @@
-function tocaSom(idElementoAudio){
+function tocaSom(SeletorAudio){
   //acessando o audio
-  document.querySelector(idElementoAudio).play();
+  const elemento = document.querySelector(SeletorAudio)
+
+  if(elemento && elemento.localName ==='audio'){
+    elemento.play();
+
+  }
+  else{
+    console.log('Elemento não encontrado ou seletor não encontrado')
+  }
   
 }
  const listaDeTeclas = document.querySelectorAll('.tecla');
 
 
 
- let contador = 0;
  // automatização
- for(contador = 0; contador < listaDeTeclas.length; contador++){
+ for(let contador = 0; contador < listaDeTeclas.length; contador++){
 
   const tecla = listaDeTeclas[contador];
   const instrumento = tecla.classList[1];
 //template string pegando os id dinamicamente
   const idAudio = `#som_${instrumento}`
-  
+
   tecla.onclick = function () {
     tocaSom(idAudio);
   }
-  tecla.onkeydown = function (){
+  tecla.onkeydown = function (evento){
+   if(evento.code === 'Space' || evento.code === 'NumpadEnter') {
     tecla.classList.add('ativa');
-
   }
-  tecla.onkeyup = function (){
-    tecla.classList.remove('ativa');
-  }
+  
+}
+tecla.onkeyup = function(){
+  tecla.classList.remove('ativa');
+}
 }
